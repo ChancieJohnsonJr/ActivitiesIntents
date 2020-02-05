ccc4d68e693dc70c93c327acc3326a05b7c83dc6
Chancie Johnson <chancie.johnson@enhanceit.us>
2020-02-05 07:16:30 -0500

    Initial commit

---
+++ app/src/main/java/com/example/activitiesintents/emi_calculator.kt
@@ -0,0 +1,15 @@
+package com.example.activitiesintents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class emi_calculator : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emi_calculator)
+    }
+
+    fun onClick(view: View) {}
+}
---
+++ app/src/main/java/com/example/activitiesintents/picture.kt
@@ -0,0 +1,15 @@
+package com.example.activitiesintents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class picture : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture)
+    }
+
+    fun onClick(view: View) {}
+}
